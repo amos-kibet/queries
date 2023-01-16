edcb2fe5d71e3cd526f7b956f1d04b843cf1d832
amos-kibet <kibetamos97@gmail.com>
2023-01-16 14:36:29 +0300

    make Ecto Queries more composable, reusable, clean & readable

--- lib/queries/accounts.ex
+++ lib/queries/accounts.ex
@@ -1,6 +1,6 @@
 defmodule Queries.Accounts do
-  alias Queries.Accounts.UserQueries
   alias Queries.Repo
+  alias Queries.Accounts.UserQueries
 
   def list_users do
     UserQueries.all()
@@ -12,14 +12,19 @@ defmodule Queries.Accounts do
     |> Repo.all()
   end
 
+  def list_users_having_posts_with_comments do
+    UserQueries.with_existing_comments()
+    |> Repo.all()
+  end
+
   def find_user_by_id(user_id) do
     UserQueries.with_id(user_id)
-    |> Repo.one()
+    |> Repo.all()
   end
 
   def user_has_posts?(user_id) do
-    UserQueries.with_existing_posts()
-    |> UserQueries.with_id(user_id)
+    UserQueries.with_id(user_id)
+    |> UserQueries.with_existing_posts()
     |> Repo.exists?()
   end
 end
--- lib/queries/accounts/user/user_queries.ex
+++ lib/queries/accounts/user/user_queries.ex
@@ -7,14 +7,41 @@ defmodule Queries.Accounts.UserQueries do
 
   def with_id(query \\ base(), id) do
     query
-    |> where([user], user.id == ^id)
+    |> where([user: user], user.id == ^id)
   end
 
   def with_existing_posts(query \\ base()) do
     query
-    |> join(:left, [user], _ in assoc(user, :posts))
-    |> where([_, posts], not is_nil(posts.id))
+    |> with_join(:posts)
+    |> where([posts: posts], not is_nil(posts.id))
   end
 
-  defp base, do: User
+  def with_existing_comments(query \\ base()) do
+    query
+    |> with_join(:comments)
+    |> where([comments: comments], not is_nil(comments.id))
+  end
+
+  defp with_join(query, :posts) do
+    if has_named_binding?(query, :posts) do
+      query
+    else
+      query
+      |> join(:left, [user: user], _ in assoc(user, :posts), as: :posts)
+    end
+  end
+
+  defp with_join(query, :comments) do
+    if has_named_binding?(query, :comments) do
+      query
+    else
+      query
+      |> with_join(:posts)
+      |> join(:left, [posts: posts], _ in assoc(posts, :comments), as: :comments)
+    end
+  end
+
+  defp base do
+    from(_ in User, as: :user)
+  end
 end
